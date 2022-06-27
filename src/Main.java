1c66bd31dbe8ea9ab756808d11e6d0cf98ba5a85
Tanya Pustovetova <plazaleto@yandex.ru>
2022-06-27 21:48:04 +0700

    5 commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
-    public static void main(String[] args)  {
-        int ticketPrice = 16000;
-        int bonus = 1;
-        int rubles = 20;
-        int finalBonus = ticketPrice / rubles * bonus;
-        System.out.println(finalBonus);
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles + "BonusMiles");
     }
 }
